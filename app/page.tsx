3ef2fe97706565dca498f6712d456791a0243ccb
Tadiwa Pfachi <pfachitadiwanashedavid@gmail.com>
2024-09-30 09:47:53 +0200

    added width

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Card className="">
+        <Card className="w-80">
           <CardHeader className="flex flex-row justify-center items-center">
             <CardTitle className="h-full w-96">tadiwapfachi</CardTitle>
             <ThemeSwitch />
