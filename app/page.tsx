546e7f24b97a45b12341a9d94aed3eda03cc5547
Tadiwa Pfachi <pfachitadiwanashedavid@gmail.com>
2024-09-30 07:47:27 +0200

    init

---
+++ app/page.tsx
@@ -0,0 +1,86 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { Switch } from "@/components/ui/switch";
+import ThemeSwitch from "@/components/themeSwitch";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  const links = [
+    { tittle: "portfolio", url: "/dev" },
+    { tittle: "about me", url: "/about" },
+    { tittle: "contact", url: "contact" },
+  ];
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <Card className="">
+          <CardHeader>
+            <CardTitle className="h-full w-96">tadiwapfachi</CardTitle>
+            <ThemeSwitch />
+          </CardHeader>
+          <Separator />
+          <CardContent>
+            {links.map((link, index) => {
+              return (
+                <div key={index}>
+                  <Link href={link.url}>
+                    <p>{link.tittle}</p>{" "}
+                  </Link>
+                </div>
+              );
+            })}
+          </CardContent>
+          <Separator />
+        </Card>
+      </main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="https://nextjs.org/icons/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Learn
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="https://nextjs.org/icons/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Examples
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="https://nextjs.org/icons/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to nextjs.org →
+        </a>
+      </footer>
+    </div>
+  );
+}
---
+++ components/themeSwitch.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { Switch } from "@/components/ui/switch";
+import { SunIcon, MoonIcon } from "lucide-react";
+import { useEffect, useState } from "react";
+
+export const ThemeSwitch = () => {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  // If the component hasn't mounted yet, return null to avoid SSR issues
+  if (!mounted) return null;
+
+  const toggleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
+  return (
+    <div className="flex items-center gap-2 p-1 rounded-md">
+      <SunIcon
+        className={`h-4 w-4 ${theme === "dark" ? "text-white" : "text-black"}`}
+      />
+      <Switch
+        id="theme-switch"
+        // checked={theme === "dark"}
+        onCheckedChange={toggleTheme}
+        aria-labelledby="theme-switch-label"
+      />
+      <MoonIcon
+        className={`h-4 w-4 ${theme === "dark" ? "text-white" : "text-black"}`}
+      />
+      <span id="theme-switch-label" className="sr-only">
+        Toggle theme
+      </span>
+    </div>
+  );
+};
+
+export default ThemeSwitch;
